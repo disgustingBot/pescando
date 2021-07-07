fb9604897390b4e6bf8857fd8f82fbcc6f5c61e1
Jonathan Cantor <jonatcantor@gmail.com>
2021-07-07 09:24:51 -0400

    Resto de widgets de estatus

--- big-data/main.php
+++ big-data/main.php
@@ -122,16 +122,113 @@
           </footer>
         </section>
 
-        <div class="widget">
+        <section class="widget widget_status">
+          <header class="widget_header">
+            <h1 class="widget_title">Estado de la granja</h1>
+          </header>
+
           <div class="donut_graph Donut_Status"></div>
-        </div>
+
+          <ul class="widget_footer">
+            <li class="widget_footer_text">
+              <img src="">
+              <p>Piscinas sin alertas</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <img src="">
+              <p>Piscinas que requieren atención</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <img src="">
+              <p>Piscinas con avisos</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <img src="">
+              <p>Inactivo</p>
+            </li>
+          </ul>
+        </section>
       </div>
 
       <div class="set_widgets set_widgets_sm">
-        <div class="widget"></div>
-        <div class="widget"></div>
-        <div class="widget"></div>
-        <div class="widget"></div>
+        <section class="widget widget_status widget_status_sm">
+          <header class="widget_header">
+            <h1 class="widget_title">pH</h1>
+            <p class="widget_footer_int">11</p>
+          </header>
+
+          <div class="donut_graph Donut_Status"></div>
+
+          <ul class="widget_footer">
+            <li class="widget_footer_text">
+              <p>Min: 0</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <p>Max: 14</p>
+            </li>
+          </ul>
+        </section>
+
+        <section class="widget widget_status widget_status_sm">
+          <header class="widget_header">
+            <h1 class="widget_title">Oxígeno</h1>
+            <p class="widget_footer_int">4,46 mg/l</p>
+          </header>
+
+          <div class="donut_graph Donut_Status"></div>
+
+          <ul class="widget_footer">
+            <li class="widget_footer_text">
+              <p>Min: 0 mg/l</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <p>Max: 10 mg/l</p>
+            </li>
+          </ul>
+        </section>
+
+        <section class="widget widget_status widget_status_sm">
+          <header class="widget_header">
+            <h1 class="widget_title">Salinidad</h1>
+            <p class="widget_footer_int">38 g/l</p>
+          </header>
+
+          <div class="donut_graph Donut_Status"></div>
+
+          <ul class="widget_footer">
+            <li class="widget_footer_text">
+              <p>Min: 0 g/l</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <p>Max: 40 g/l</p>
+            </li>
+          </ul>
+        </section>
+
+        <section class="widget widget_status widget_status_sm">
+          <header class="widget_header">
+            <h1 class="widget_title">Temperatura</h1>
+            <p class="widget_footer_int">20° C</p>
+          </header>
+
+          <div class="donut_graph Donut_Status"></div>
+
+          <ul class="widget_footer">
+            <li class="widget_footer_text">
+              <p>Min: 0° C</p>
+            </li>
+
+            <li class="widget_footer_text">
+              <p>Max: 40° C</p>
+            </li>
+          </ul>
+        </section>
       </div>
     </div>
   </main>
