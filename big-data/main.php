32c1e9c3b6e2c3521fc5eb609480567d3590ea06
Sofia <molinerozadkiel@gmail.com>
2021-08-09 17:42:04 +0200

    some fixes

--- big-data/main.php
+++ big-data/main.php
@@ -13,6 +13,7 @@
   $piscinas = get_piscinas();
   $videos = get_videos_big_data();
 
+  $redirect_time = 60;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,7 +48,8 @@
 
         <div class="viday">
           <div class="viday_media">
-            <video class="viday_video" poster="" onclick="setCookie('show', 'video', 1);setCookie('slug', '<?= $video['slug'] ?>', 1);setCookie('lang', '<?= $_SESSION['lang'] ?>', 1)">
+            <video class="viday_video" poster="" onclick="video_click('<?= $video['slug'] ?>', '<?= $_SESSION['lang'] ?>')">
+            <!-- <video class="viday_video" poster="" onclick="setCookie('show', 'video', 1);setCookie('slug', '<?= $video['slug'] ?>', 1);setCookie('lang', '<?= $_SESSION['lang'] ?>', 1);altClassFromSelector('', 'div.screen_pools_interactive', ['screen_pools_interactive', 'rowcol1'])"> -->
               <!-- <source src="" type="video/mp4"> -->
             </video>
           </div>
@@ -66,13 +68,22 @@
           let piscinas = <?= json_encode($piscinas) ?>;
           piscinas.forEach( piscina => {
             document.querySelector('#'+piscina.slug).onclick = _ => {
-              altClassFromSelector('selected', '#'+piscina.slug)
+              altClassFromSelector(piscina.slug, 'div.screen_pools_interactive', ['screen_pools_interactive', 'rowcol1'])
+              // altClassFromSelector('selected', '#'+piscina.slug)
               setCookie('show', 'piscina', 1);
               setCookie('slug', piscina.slug, 1);
               setCookie('lang', '<?=$_SESSION["lang"]?>', 1);
             }
           });
           </script>
+          <?php foreach ($piscinas as $piscina) { ?>
+            <style>
+            <?= ".screen_pools_interactive.$piscina[slug] #$piscina[slug] path" ?>{
+                stroke: #d6001c;
+                fill: #0875bd36;
+            }
+            </style>
+          <?php } ?>
       </div>
     </div>
   </main>
