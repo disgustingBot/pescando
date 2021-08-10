b50fd4b820bc169f9e3fabba466f14a023c0fd7c
Sofia <molinerozadkiel@gmail.com>
2021-08-10 23:51:50 +0200

    syncro

--- cultivo/inc.session.end.php
+++ cultivo/inc.session.end.php
@@ -1,9 +1,9 @@
 <?php
-  session_start();
-  
-  $_SESSION = array();
-  session_destroy();
-    
-  Header("Location: index.php");
-  die;
+session_start();
+
+$_SESSION = array();
+session_destroy();
+
+Header("Location: index.php");
+die;
 ?>
